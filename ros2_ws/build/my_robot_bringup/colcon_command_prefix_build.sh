6455cb76671cef1041eb889389e903ef4506f0c4
VijayVikasM <vijayvikasmangen@gmail.com>
2024-11-25 02:17:58 +0100

    turtlesim project files

---
+++ ros2_ws/build/my_robot_bringup/colcon_command_prefix_build.sh
@@ -0,0 +1,6 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/vijayvikasm/ros2_ws/install/my_robot_interfaces/share/my_robot_interfaces/package.sh"
+. "/home/vijayvikasm/ros2_ws/install/my_cpp_pkg/share/my_cpp_pkg/package.sh"
+. "/home/vijayvikasm/ros2_ws/install/my_py_pkg/share/my_py_pkg/package.sh"
+. "/home/vijayvikasm/ros2_ws/install/turtlesim_catch_them_all/share/turtlesim_catch_them_all/package.sh"
+. "/home/vijayvikasm/ros2_ws/install/turtlesim_project_cpp/share/turtlesim_project_cpp/package.sh"
--- ros2_ws/src/turtlesim_catch_them_all/setup.py
+++ ros2_ws/src/turtlesim_catch_them_all/setup.py
@@ -1,11 +1,11 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 package_name = 'turtlesim_catch_them_all'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
@@ -13,13 +13,15 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='vijayvikasm',
-    maintainer_email='vijayvikasmangen@gmail.com',
+    maintainer='ed',
+    maintainer_email='ed@todo.todo',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            "turtlesim_controller = turtlesim_catch_them_all.turtle_controller:main",
+            "turtle_spawner = turtlesim_catch_them_all.turtle_spawner:main"
         ],
     },
 )
